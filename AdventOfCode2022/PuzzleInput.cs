cb36453f808b30bd166e1f05537720c4eee9e3b3
bramv <bramv@DESKTOP-BRAM>
2023-10-25 16:16:46 +0200

    Add project files.

---
+++ AdventOfCode2022/PuzzleInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class PuzzleInput
+    {
+        public PuzzleInput(int day)
+        {
+            Array exampleInput = ;
+            Array puzzleInput = 
+
+        }
+
+    }
+}
