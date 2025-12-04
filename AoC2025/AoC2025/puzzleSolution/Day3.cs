9410be4f73ddb7941aa1338ccfbcd7deca2d1910
bramv <bramv@DESKTOP-BRAM>
2025-12-04 21:51:24 +0100

    Day 3 and 4

---
+++ AoC2025/AoC2025/puzzleSolution/Day3.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+class AoC2025Day3
+{
+
+    public static void Part1()
+    {
+        int day = 3;
+        long answer = 0;
+
+        string[] input = readInput(day, false);
+
+        foreach (string line in input)
+        {
+            int[] values = line.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
+
+            int firstMaxValue = values.SkipLast(1).Max(); //we need two batteries
+            int firstMaxIndex = Array.IndexOf(values, firstMaxValue); 
+
+            //check subset of array
+            int[] subset = values.Skip(firstMaxIndex + 1).ToArray();
+            int secondMaxValue = subset.Max();
+
+            int result = int.Parse(firstMaxValue.ToString() + secondMaxValue.ToString());
+
+            answer += result;
+        }
+
+        Console.WriteLine($"The answer of day {day} = {answer}");
+    }
+
+    public static void Part2()
+    {
+        int day = 3;
+        long answer = 0;
+
+        string[] input = readInput(day, false);
+
+        foreach (string line in input)
+        {
+            int[] values = line.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
+
+            int firstMaxValue = values.SkipLast(11).Max(); //we need 12 batteries
+            int firstMaxIndex = Array.IndexOf(values, firstMaxValue);
+
+            int[] subset = values.Skip(firstMaxIndex + 1).ToArray();
+            string subResult = firstMaxValue.ToString();
+
+            //check subset of array
+            for (int i = 1; i < 12; i++)
+            {
+                int nextMaxValue = subset.SkipLast(11-i).Max();
+                int nextMaxIndex = Array.IndexOf(subset, nextMaxValue);
+
+                subset = subset.Skip(nextMaxIndex + 1).ToArray();
+                subResult += nextMaxValue.ToString();
+            }
+
+            long result = long.Parse(subResult);
+            Console.WriteLine($"Intermediate result: {result}");
+            answer += result;
+        }
+
+        Console.WriteLine($"The answer of day {day} = {answer}");
+    }
+
+    public static string[] readInput(int day, bool useExample)
+    {
+        if (useExample)
+        {
+            string[] exampleInput = File.ReadAllLines($"./puzzleInputFiles/exampleInputDay{day}.txt");
+            return exampleInput;
+        }
+        else
+        {
+            string[] puzzleInput = File.ReadAllLines($"./puzzleInputFiles/puzzleInputDay{day}.txt");
+            return puzzleInput;
+        }
+    }
+
+}
+
+
---
+++ AoC2025/AoC2025/puzzleSolution/Day4.cs
@@ -0,0 +1,127 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
+using static System.Net.Mime.MediaTypeNames;
+
+class AoC2025Day4
+{
+
+    public static void Part1()
+    {
+        int day = 4;
+        long answer = 0;
+
+        string[] input = readInput(day, false);
+
+        //Add outer lines array for easier processing
+        var processInput = new List<string>();
+        processInput.Add(new string('0', input[0].Length + 2)); //firstline
+
+        foreach (string line in input)
+        {
+            string extendedLine = "0" + line + "0";
+            processInput.Add(extendedLine);
+        }
+        
+        processInput.Add(new string('0', input[0].Length + 2)); //lastline
+
+        //Process updated input
+
+        for (int i = 0; i < processInput.Count - 1; i++)
+        {
+            for (int j = 0; j < processInput[i].Length - 1; j++)
+            {
+                char currentChar = processInput[i][j];
+                              
+                if (currentChar == '@')
+                {
+                    int startIndexCheck = j - 1;
+                    string aboveLine = processInput[i - 1].Substring(startIndexCheck, 3);
+                    string belowLine = processInput[i + 1].Substring(startIndexCheck, 3);
+                    string currentLine = processInput[i].Substring(startIndexCheck, 3);
+
+                    string combinedText = aboveLine + belowLine + currentLine;
+
+                    if (combinedText.Count(c => c == '@') < 5) //Including itself max value is 5
+                    {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"The answer of day {day} = {answer}");
+    }
+
+    public static void Part2()
+    {
+        int day = 4;
+        long answer = 0;
+
+        string[] input = readInput(day, false);
+
+        //Add outer lines array for easier processing
+        var processInput = new List<string>();
+        processInput.Add(new string('0', input[0].Length + 2)); //firstline
+
+        foreach (string line in input)
+        {
+            string extendedLine = "0" + line + "0";
+            processInput.Add(extendedLine);
+        }
+
+        processInput.Add(new string('0', input[0].Length + 2)); //lastline
+
+        //Process updated input
+
+        answer = 0;
+        long lastAnswer = -1;
+
+        while (lastAnswer != answer)
+        {
+            lastAnswer = answer;
+
+            for (int i = 0; i < processInput.Count - 1; i++)
+            {
+                for (int j = 0; j < processInput[i].Length - 1; j++)
+                {
+                    char currentChar = processInput[i][j];
+
+                    if (currentChar == '@')
+                    {
+                        int startIndexCheck = j - 1;
+                        string aboveLine = processInput[i - 1].Substring(startIndexCheck, 3);
+                        string belowLine = processInput[i + 1].Substring(startIndexCheck, 3);
+                        string currentLine = processInput[i].Substring(startIndexCheck, 3);
+
+                        string combinedText = aboveLine + belowLine + currentLine;
+
+                        if (combinedText.Count(c => c == '@') < 5) //Including itself max value is 5
+                        {
+                            processInput[i] = processInput[i].Remove(j, 1).Insert(j, "0");
+                            answer++;
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"The answer of day {day} = {answer}");
+    }
+
+    public static string[] readInput(int day, bool useExample)
+    {
+        if (useExample)
+        {
+            string[] exampleInput = File.ReadAllLines($"./puzzleInputFiles/exampleInputDay{day}.txt");
+            return exampleInput;
+        }
+        else
+        {
+            string[] puzzleInput = File.ReadAllLines($"./puzzleInputFiles/puzzleInputDay{day}.txt");
+            return puzzleInput;
+        }
+    }
+
+}
+
+
